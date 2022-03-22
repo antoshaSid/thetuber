2914c0a39aa619e0e75c112b425df4e95d4514b4
Anton Sidlyar <antonsidlyar@gmail.com>
2022-03-22 02:06:05 +0200

    Init front-end

--- src/main/java/com/thetuber/thetuber/controller/PersonController.java
+++ src/main/java/com/thetuber/thetuber/controller/PersonController.java
@@ -1,29 +1,25 @@
 package com.thetuber.thetuber.controller;
 
-import com.thetuber.thetuber.entity.Person;
 import com.thetuber.thetuber.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
-@RestController
-@RequestMapping(value = "/persons")
+@Controller
+@RequestMapping(value = "/")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
     @GetMapping
-    public ResponseEntity<List<Person>> getAllPersons() {
-        List<Person> persons = personService.getAllPersons();
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .body(persons);
+    public ModelAndView getAllPersons() {
+        ModelAndView modelAndView = new ModelAndView("home", HttpStatus.OK);
+        modelAndView.addObject(personService.getAllPersons());
+        return modelAndView;
     }
 
 
